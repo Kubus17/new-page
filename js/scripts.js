d8ec80015e54ba2e40de60ecd46e7926331f4258
LAPTOP-57J009DG\piotr <kubus-o_2@tlen.pl>
2018-01-15 10:53:01 +0100

    Konfiguracja button socjalmedia

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-scritp
+var newTwitBtn = document.getElementById('btntwitter');
+var newFaceBtn = document.getElementById('btnfacebook');
+var newGoogleBtn = document.getElementById('btngoogle');
+var newInstBtn = document.getElementById('btninstagram');
+newTwitBtn.addEventListener("click", mytwit);
+newFaceBtn.addEventListener("click", myface);
+newGoogleBtn.addEventListener("click", mygoog);
+newInstBtn.addEventListener("click", myins);
+
+function mytwit() {
+    window.open('https://twitter.com/?lang=pl', '_blank');
+}
+function myface() {
+    window.open('https://facebook.com/?lang=pl', '_blank');
+}
+function mygoog() {
+    window.open('https://google.com/?lang=pl', '_blank');
+}
+function myins() {
+    window.open('https://instagram.com/?hl=pl', '_blank');
+}
